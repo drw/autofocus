a334b45ecd3b4febe293d096c120ecb01552feda
drw <drw@users.noreply.github.com>
2018-07-02 21:06:24 -0400

    Refactor focus.py to allow the first command-line argument to specify a user to focus on

--- focus.py
+++ focus.py
@@ -1,4 +1,4 @@
-import re, json, requests
+import sys, re, json, requests
 from datetime import datetime, timedelta
 import tweepy
 from pprint import pprint
@@ -24,26 +24,46 @@ def elevate(tweet):
 
     return True
 
-consumer_key = TWITTER_KEYS['consumer_key']
-consumer_secret = TWITTER_KEYS['consumer_secret']
-access_token_key = TWITTER_KEYS['access_token_key']
-access_token_secret = TWITTER_KEYS['access_token_secret']
+def main(*args,**kwargs):
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token_key, access_token_secret)
+    target_user = kwargs.get('target_user',None)
+    consumer_key = TWITTER_KEYS['consumer_key']
+    consumer_secret = TWITTER_KEYS['consumer_secret']
+    access_token_key = TWITTER_KEYS['access_token_key']
+    access_token_secret = TWITTER_KEYS['access_token_secret']
 
-api = tweepy.API(auth)
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token_key, access_token_secret)
 
-public_tweets = api.home_timeline(count = 40, tweet_mode = 'extended')
+    api = tweepy.API(auth)
 
-blocked_count = 0
-for tweet in public_tweets:
-    if elevate(tweet):
-        print("{} - {} [{}/{}]: {}".format(tweet.user.screen_name,tweet.created_at,tweet.favorite_count,tweet.retweet_count,tweet.full_text))
+    if target_user is None:
+        tweets = api.home_timeline(count = 40, tweet_mode = 'extended') # public tweets
     else:
-        blocked_count += 1
+        tweets = api.user_timeline(id = target_user, count = 40, tweet_mode = 'extended')
 
-print(dir(public_tweets[0]))
-print("Blocked {} of {} posts.".format(blocked_count, len(public_tweets)))
-# api.update_status( msg) 
+    blocked_count = 0
+    for tweet in tweets:
+        if elevate(tweet):
+            print("{} - {} [{}/{}]: {}".format(tweet.user.screen_name,tweet.created_at,tweet.favorite_count,tweet.retweet_count,tweet.full_text))
+            #if tweet.user.screen_name == 'a_baronca':
+            #    for k in dir(tweet):
+            #        if k[0] != '_' and k not in ['retweets']:
+            #            print(k)
+            #            pprint(getattr(tweet,k))
+        else:
+            blocked_count += 1
 
+    print(dir(tweets[0]))
+    print("Blocked {} of {} posts.".format(blocked_count, len(tweets)))
+    # api.update_status( msg) 
+
+    # tweet.entities.hashtags should be a list of tags
+
+# sys.argv[1] specifies user to focus on
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        target_user = sys.argv[1]
+        main(target_user = target_user)
+    else:
+        main()
